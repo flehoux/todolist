12d186f0b6cd1215bfe32e0d924d164a780164c2
Frank Lehoux <f.lehoux2@gmail.com>
2014-06-29 17:39:44 -0400

    Modified devise views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def display_messages(error, notice, alert)
+    content_tag :div, :class => "message_box" do
+      content_tag(:p, flash[:error], :class => "error small-text error-text") if error
+      content_tag(:p, flash[:notice], :class => "notice small-text") if notice
+      content_tag(:p, flash[:alert], :class => "alert small-text error-text") if alert
+    end if error || notice || alert
+  end
 end
