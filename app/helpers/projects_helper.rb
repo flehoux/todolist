1f953381871824009ddf593cbcdcf84ec7c6098f
Frank Lehoux <f.lehoux2@gmail.com>
2014-07-09 16:00:15 -0400

    Text-area for descriptions

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -7,4 +7,12 @@ module ProjectsHelper
       edit_project_path(project)
     end
   end
+
+  def project_back_link(project)
+    if current_page?(new_project_path)
+      projects_path
+    elsif current_page?(edit_project_path)
+      project_path(project)
+    end
+  end
 end
