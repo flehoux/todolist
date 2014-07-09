bad0285a743b16a20bff74a16b0a5ac443230604
Frank Lehoux <f.lehoux2@gmail.com>
2014-07-09 15:26:27 -0400

    Modified project row layout

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+
+  def project_link(project)
+    if current_page?(projects_path)
+      project_path(project)
+    elsif current_page?(project_path(project))
+      edit_project_path(project)
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 120 }
   validates :category, presence: true, :inclusion => 1..3
 end
