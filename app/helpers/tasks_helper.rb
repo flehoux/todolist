d904eee9cbe15df65ff80160fa1f2cfa9873d580
Frank Lehoux <f.lehoux2@gmail.com>
2014-07-09 11:27:57 -0400

    Dynamic previous-next buttons

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -7,4 +7,36 @@ module TasksHelper
       @task.category
     end
   end
+
+  def previous_category_button(task)
+    return "" if task.category == 1
+
+    link = link_to "<", project_task_path(@project, task.id, task: { category: task.category - 1 }), method: :patch
+
+    html = <<-HTML
+    <div class="previous-cat">
+      <div class="icon icon-text previous-icon">
+          #{link}
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def next_category_button(task)
+    return "" if task.category == 3
+
+    link = link_to ">", project_task_path(@project, task.id, task: { category: task.category + 1 }), method: :patch
+
+    html = <<-HTML
+    <div class="next-cat">
+      <div class="icon icon-text next-icon">
+          #{link}
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
