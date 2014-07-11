54c60046861191efa0374c28bde82d2b7403b6e0
Frank Lehoux <f.lehoux2@gmail.com>
2014-07-11 12:06:09 -0400

    Change root URL to projects

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :tasks, only: [:new, :create, :edit, :update, :destroy]
   end
   get '/contact',   to: 'static_pages#contact'
-  root  to: 'static_pages#home'
+  root  to: 'projects#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
-
-  end
-
   describe "Contact page" do
 
   end
